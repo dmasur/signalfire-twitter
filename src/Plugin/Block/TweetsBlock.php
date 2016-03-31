5c020e521dfce9d64bf418822ba14d1109b5d46d
Robert Coster <rob@signalfire.co.uk>
2016-03-31 15:14:21 +0100

    Change to add max-age for caching

--- src/Plugin/Block/TweetsBlock.php
+++ src/Plugin/Block/TweetsBlock.php
@@ -25,7 +25,7 @@ class TweetsBlock extends BlockBase {
     // Create a variable used to indicate how many
     // of the required keys have been found
     $keys_found = 0;
-    
+
     // Create an array containing the identifier of the
     // array element required
     $required_keys = [
@@ -36,7 +36,7 @@ class TweetsBlock extends BlockBase {
       'tweet_count',
       'screen_name'
     ];
-    
+
     // Iterate the required keys and get the configuration value
     // from Drupal. If the configuration value is found then icrement
     // the counter
@@ -46,15 +46,15 @@ class TweetsBlock extends BlockBase {
         $keys_found++;
       }
     }
-    
+
     // Check the number of keys found === the number of keys required
     // If the same then return true, else false
-    return $keys_found === count($required_keys); 
+    return $keys_found === count($required_keys);
 
   }
 
   private function convert_atsigns($tweet){
-    return preg_replace("/@([A-Za-z0-9\/\.]*)/", "<a href=\"http://www.twitter.com/$1\">@$1</a>", $tweet);    
+    return preg_replace("/@([A-Za-z0-9\/\.]*)/", "<a href=\"http://www.twitter.com/$1\">@$1</a>", $tweet);
   }
 
   private function convert_hashtags($tweet){
@@ -62,20 +62,20 @@ class TweetsBlock extends BlockBase {
   }
 
   private function convert_links($tweet){
-    return preg_replace("/([\w]+\:\/\/[\w-?&;#~=\.\/\@]+[\w\/])/", "<a target=\"_blank\" href=\"$1\">$1</a>", $tweet);    
+    return preg_replace("/([\w]+\:\/\/[\w-?&;#~=\.\/\@]+[\w\/])/", "<a target=\"_blank\" href=\"$1\">$1</a>", $tweet);
   }
 
   private function format_tweet_with_links($tweet){
 
     // Convert any links in the text to hyperlinks
     $tweet = $this->convert_links($tweet);
-    
+
     // Convert any #tags in the tweet text to hyperlinks
     $tweet = $this->convert_hashtags($tweet);
-    
+
     // Convert any @signs in the tweet text to hyperlinks
     $tweet = $this->convert_atsigns($tweet);
-    
+
     // Return the formatted text
     return $tweet;
   }
@@ -84,7 +84,7 @@ class TweetsBlock extends BlockBase {
 
     // Return a unique id for the caching
     return 'signalfire_twitter_tweets';
- 
+
   }
 
   private function cache_expire(){
@@ -95,40 +95,51 @@ class TweetsBlock extends BlockBase {
 
     // Create array for tweets
     $tweets = [];
-    
+
     // See if the tweets are already cached
     if ($cache = \Drupal::cache()->get($this->cache_id())){
 
       // Set the tweets array to contain the cached data
       $tweets = $cache->data;
-    
+
     }else{
-    
-      // Tweets not cached so connect to twitter
-      $connection = new TwitterOAuth(
-        $config->get('consumer_key'),
-        $config->get('consumer_secret'),
-        $config->get('access_token'),
-        $config->get('access_token_secret')
-      );
 
-      // Get the tweets for the user
-      $tweets = $connection->get(
-        'statuses/user_timeline', 
-        array(
-          'count' => $config->get('tweet_count'),
-          'screen_name' => $config->get('screen_name'), 
-          'exclude_replies' => true
-        )
-      );
+      // Attempt to fetch tweets
+      try{
 
-      // Add the retrieved tweets to the cache and expire after
-      // the timestamp (set as 10 minutes currently)
-      \Drupal::cache()->set($this->cache_id(), $tweets, $this->cache_expire());
-    } 
+        // Tweets not cached so connect to twitter
+        $connection = new TwitterOAuth(
+          $config->get('consumer_key'),
+          $config->get('consumer_secret'),
+          $config->get('access_token'),
+          $config->get('access_token_secret')
+        );
+
+        // Get the tweets for the user
+        $tweets = $connection->get(
+          'statuses/user_timeline',
+          array(
+            'count' => $config->get('tweet_count'),
+            'screen_name' => $config->get('screen_name'),
+            'exclude_replies' => true
+          )
+        );
+
+        // Add the retrieved tweets to the cache and expire after
+        // the timestamp (set as 10 minutes currently)
+        \Drupal::cache()->set($this->cache_id(), $tweets, $this->cache_expire());
+
+      }catch (Exception $ex){
+
+        // Do something
+        drupal_set_message(t('An error occurred, was unable to fetch tweets'), 'error');
+
+      }
+
+    }
 
     // Return the tweets
-    return $tweets; 
+    return $tweets;
 
   }
 
@@ -142,25 +153,25 @@ class TweetsBlock extends BlockBase {
 
       // Load tweets either from cache or live
       $tweets = $this->load_tweets($config);
-      
+
       // Declare an elements array, setting its theme to item_list
       // and the type of item_list as ul
       $elements = [];
       $elements['#theme'] = 'item_list';
-      $elements['#type'] = 'ul';        
-      
+      $elements['#type'] = 'ul';
+
       // Check if there are any tweets to display
       if (count($tweets) > 0){
 
         // There are tweets so iterate over then
-        foreach($tweets as $tweet){            
-      
+        foreach($tweets as $tweet){
+
           // Create an item in the array for the tweet, formatting the
           // links etc in the tweet and setting a class of tweet on the li
           $elements['#items'][] = [
             '#markup' => sprintf(
               '<span class="text">%s</span><span class="date">%s</span>',
-              $this->format_tweet_with_links($tweet->text), 
+              $this->format_tweet_with_links($tweet->text),
               date('jS F Y H:i',strtotime($tweet->created_at))
             ),
             '#wrapper_attributes' => ['class'=>'tweet']
@@ -169,7 +180,7 @@ class TweetsBlock extends BlockBase {
         }
 
       }else{
-        
+
         // There are no tweets in the $tweet array so display message and add
         // no-tweets class
         $elements['#items'][] = [
@@ -197,6 +208,7 @@ class TweetsBlock extends BlockBase {
    */
   public function build() {
     $build = [];
+    $build['#cache'] = ['max-age'=>0];
     $build['tweets_block']['#markup'] = $this->tweet_markup();
     return $build;
   }
